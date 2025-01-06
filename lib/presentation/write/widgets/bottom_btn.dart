dc1ab356563aeb7d7ffe8191541f86cb606fac46
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-06 15:04:21 +0900

    feat: 글작성 페이지 위젯 분리를 통한 모듈화

---
+++ lib/presentation/write/widgets/bottom_btn.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+Widget bottomBtn(contentController, tagController, double bottomPadding) {
+  return Padding(
+    padding: EdgeInsets.only(
+      left: 20.0, // 좌측 여백
+      right: 20.0, // 우측 여백
+      bottom: bottomPadding, // 버튼을 하단에서 띄우는 여백
+    ),
+    child: ElevatedButton(
+      onPressed: () {
+        print('Button Pressed!');
+        print(contentController.text);
+        print(tagController.text);
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFF54A6FF), // 배경 색상
+        shadowColor: Colors.black, // 그림자 색상
+        elevation: 5, // 그림자 높이
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 10),
+      ),
+      child: const Text(
+        '공유',
+        style: TextStyle(
+          fontSize: 18,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ), // 텍스트 스타일
+      ),
+    ),
+  );
+}
