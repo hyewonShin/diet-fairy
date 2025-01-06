eb3c2ad56e69140c03c3665b115343d01d78af34
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-06 20:30:07 +0900

    feat: 글작성 페이지에서 공통으로 사용 되는 image_box 구현

---
+++ lib/presentation/write/widgets/img_container.dart
@@ -0,0 +1,20 @@
+import 'package:diet_fairy/presentation/write_img_upload/upload_controller.dart';
+import 'package:diet_fairy/presentation/write_img_upload/widgets/image_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+Obx imgContainer(UploadController controller, double screenHeight) {
+  return Obx(() {
+    return controller.selectedImage.value != null
+        ? ImagePreview(
+            image: controller.selectedImage.value!, screenHeight: screenHeight)
+        : Container(
+            height: screenHeight / 2,
+            width: double.infinity,
+            decoration: const BoxDecoration(color: Colors.white),
+            child: const Center(
+              child: Text('이미지를 선택해주세요'),
+            ),
+          );
+  });
+}
