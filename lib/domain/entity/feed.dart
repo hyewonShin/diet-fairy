9142b3c29818293a00bc59179720194d932947a1
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-07 14:34:13 +0900

    feat: Feed Entity 생성

--- lib/domain/entity/feed.dart
+++ lib/domain/entity/feed.dart
@@ -1,5 +1,22 @@
 class Feed {
-  String id;
+  int id;
+  String userId;
   String imageUrl;
-  Feed({required this.id, required this.imageUrl});
+  List<String>? tag;
+  String content;
+  DateTime createdAt;
+  //List<int>? comment
+  int likeCnt = 0;
+  bool isLike = false;
+
+  Feed({
+    required this.id,
+    required this.userId,
+    required this.imageUrl,
+    this.tag,
+    required this.content,
+    required this.createdAt,
+    required this.likeCnt,
+    required this.isLike,
+  });
 }
