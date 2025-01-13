0d12ff7377b4113c6f8babc4bb92ce83809f5252
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-13 09:57:09 +0900

    feat: 삭제 버튼 터치 시, 컨펌 alert 생성

--- lib/presentation/home/widgets/home_feed_delete_button.dart
+++ lib/presentation/home/widgets/home_feed_delete_button.dart
@@ -1,4 +1,5 @@
 import 'package:diet_fairy/presentation/home/home_view_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,7 +12,34 @@ class HomeFeedDeleteButton extends ConsumerWidget {
     return GestureDetector(
       onTap: () async {
         try {
-          await ref.read(homeViewModelProvider.notifier).deleteFeed(feedId);
+          showCupertinoDialog(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: const Text(
+                    '정말 삭제하시겠습니까?',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  actions: [
+                    CupertinoDialogAction(
+                        isDefaultAction: true,
+                        onPressed: () async {
+                          print('삭제 완료');
+                          await ref
+                              .read(homeViewModelProvider.notifier)
+                              .deleteFeed(feedId);
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('확인')),
+                    CupertinoDialogAction(
+                        isDestructiveAction: true,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('취소'))
+                  ],
+                );
+              });
         } catch (e) {
           print('피드 삭제 중 오류 발생: $e');
         }
